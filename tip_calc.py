fd2953d30c7d29544a84fabdd8153fb5f6fc51e8
Timothy Truty <ttruty@gmail.com>
2025-03-14 07:56:18 -0500

    Create tip_calc.py

---
+++ tip_calc.py
@@ -0,0 +1,19 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    # convert dollar string to float
+    pass # remove this line when you start coding
+
+
+
+def percent_to_float(p):
+    # convert percent string to float
+    pass # remove this line when you start coding
+
+
+main()
